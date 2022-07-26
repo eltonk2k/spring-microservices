f67401b5d6321ea85db8bda346a2999d972a7763
eltonk2k <elieltonpb1@gmail.com>
2022-07-26 20:57:30 -0300

    Criando a Base de Dados e Adicionando as Dependencias do Spring Data JPA

--- cambio-service/src/main/java/br/com/example/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/example/controller/CambioController.java
@@ -2,6 +2,7 @@ package br.com.example.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import br.com.example.model.Cambio;
 @RequestMapping("cambio-service")
 public class CambioController {
 	
+	@Autowired
 	private Environment environment;
 	
 	
