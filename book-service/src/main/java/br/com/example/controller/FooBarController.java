270ffd7d3e3f09a398a381e26162a02333de0237
eltonk2k <elieltonpb1@gmail.com>
2022-07-29 18:59:23 -0300

    Circuit Breaker com Resilience4j

---
+++ book-service/src/main/java/br/com/example/controller/FooBarController.java
@@ -0,0 +1,18 @@
+package br.com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+	
+	
+	@GetMapping("/foo-bar")
+	public String fooBar() {
+		return "FooBar!!!";
+	}
+	
+
+}
