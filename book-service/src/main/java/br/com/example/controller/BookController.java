eeeccb3aa0eb15bfb83a11213c62326b7e4ac52a
eltonk2k <elieltonpb1@gmail.com>
2022-08-02 22:49:32 -0300

    Conectando o Cambio Microservice e o API Gateway ao Zipkin

--- book-service/src/main/java/br/com/example/controller/BookController.java
+++ book-service/src/main/java/br/com/example/controller/BookController.java
@@ -13,37 +13,36 @@ import br.com.example.repository.BookRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name="Book endpoint")
+@Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
-	
+
 	@Autowired
 	private Environment environment;
-	
+
 	@Autowired
 	private BookRepository repository;
-	
+
 	@Autowired
 	private CambioProxy proxy;
-	
-	
+
 	@Operation(summary = "Find a specific book by your ID")
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
+
 		var book = repository.getById(id);
-		if (book == null) {
+		if (book == null)
 			throw new RuntimeException("Book not Found");
-		}
-		
-		
+
 		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
+
 		var port = environment.getProperty("local.server.port");
 		book.setEnvironment("Book port: " + port + " Cambio Port " + cambio.getEnvironment());
 		book.setPrice(cambio.getConvertedValue());
 		return book;
 	}
-	
+
 //	@GetMapping(value = "/{id}/{currency}")
 //	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
 //		var book = repository.getById(id);
